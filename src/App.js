a6311f232be46802820f6a926cced2b5c810aea9
JSShant <joshua.shanthikumar@outlook.com>
2022-08-11 10:38:50 +1000

    Creating Of header component with dark theme

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Homepage from "./Pages/Homepage";
 import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
         <Route path="/" component={Homepage} exact />
         <Route path="/coins/:id" component={CoinPage} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,66 @@
+import {
+  AppBar,
+  Container,
+  createTheme,
+  makeStyles,
+  MenuItem,
+  Select,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push("/")}
+              className={classes.title}
+              variant="h6"
+            >
+              Crypto-Tracker
+            </Typography>
+
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"AUD"}>AUD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
